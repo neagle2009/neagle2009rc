4915dcadce4e4f4ac23415003c4a8bd7d0afac15
neagle <neagle@didideMacBook-Air-2.local>
2016-05-27 23:13:47 +0800

    CHG: do someting like better

--- auto.sh
+++ auto.sh
@@ -1,26 +1,37 @@
 #! /bin/bash
+function getBashrcName() {
+    echo ${NEAGLERC_URL}
+    return
+    if [ "$(uname)" == "Darwin" ]; then
+        echo ".profile" 
+        return 
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        echo ".bashrc" 
+        return
+    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+        echo ".bashrc" 
+        return
+    fi
+        echo ".bashrc" 
+        return
+}
+profileRc="$HOME/`getBashrcName`"
 
-zipfile="master.zip"
+# download AND unzip
+zipfile="neagle2009rc-master.zip"
 wget -O "${zipfile}" -c 'https://github.com/neagle2009/neagle2009rc/archive/master.zip' || exit
 if [ ! -f "${zipfile}" ] ;then
     echo "not found file ${zipfile}"
     exit
 fi
-
 unzip "${zipfile}"
 
-dir="neagle2009rc-master"
-cat ${dir}/bashrc_public >> ~/.bashrc
-cat ${dir}/vimrc/vimrc >> ~/.vimrc
+### config bash 
+neaglerc2009Dir="neagle2009rc-master"
+cat ${neaglerc2009Dir}/bashrc_public >> ${profileRc}
 colorFile="$HMOE/.dircolors"
 if [ ! -f "${colorFile}" ] ;then
-    cat ${dir}/dir_colors >> ${colorFile}
-fi
-
-vimdir="$HOME/.vim"
-
-if [ ! -d "$vimdir" ] ;then
-    mkdir "${vimdir}"
+    cat ${neaglerc2009Dir}/dir_colors >> ${colorFile}
 fi
 
 hideBinDir="$HOME/.bin"
@@ -28,12 +39,21 @@ if [ -d "${hideBinDir}" ] ;then
     mkdir "${hideBinDir}"
 fi
 
-cp -r "${dir}/vimrc/plugin" "${vimdir}/plugin"
+### config vim
+cat ${neaglerc2009Dir}/vimrc/vimrc >> ~/.vimrc
+vimdir="$HOME/.vim"
+if [ ! -d "$vimdir" ] ;then
+    mkdir "${vimdir}"
+fi
+cp -r "${neaglerc2009Dir}/vimrc/plugin" "${vimdir}/plugin"
 
-# del 
-rm -rf "${dir}"
-rm "$0"
+
+### del file 
+rm -rf "${neaglerc2009Dir}"
+rm "$0" #delete self
 
 ##download cdargs
-wget -c 'https://github.com/cbxbiker61/cdargs/archive/master.zip'
+zipCdargsFile="cdargs.zip"
+wget -c 'https://github.com/cbxbiker61/cdargs/archive/master.zip' -O ${zipCdargsFile}
+unzip ${zipCdargsFile}
 wget -c 'http://neagle.qiniudn.com/neagle/2014/11/29/cdargs-bash.sh'
