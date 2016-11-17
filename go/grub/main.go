d46519d7a18f19c029179b8ffcf1f2cf373c9b34
neagle <gu217@126.com>
2016-11-17 23:17:17 +0800

    CHG: 添加缓存层

--- go/grub/main.go
+++ go/grub/main.go
@@ -13,11 +13,14 @@ import (
 
 const (
 	BASEURL  = "http://www.bz55.com"
-	DOWNPATH = "/Users/didi/goproj/src/grub/image/"
-    CHANNEL_CACHE_LENGTH = 20
+    //DOWNPATH = "/Users/didi/goproj/src/grub/image/"
+	DOWNPATH = "/home/neagle/goproj/src/grub/image/"
+    CHANNEL_CACHE_LENGTH = 10
+    CHANNEL_DOWN_QUEUE_LENGTH = 2
 )
 
 var urlChl chan string
+var downChl chan string
 
 func findElement(pageUrl string) {
 	doc, err := goquery.NewDocument(pageUrl)
@@ -28,9 +31,8 @@ func findElement(pageUrl string) {
 	doc.Find("p").Eq(2).Find("img").Each(func(i int, s *goquery.Selection) {
 		src, find := s.Attr("src")
 		if find {
-			logOut("parse order: " + src)
 			urlChl <- src
-			downFile(src)
+			logOut(fmt.Sprintf("parse down url: %s, channel length: %d" ,src, len(urlChl)))
 		}
 	})
 }
@@ -55,20 +57,32 @@ func downFile(fileUrl string) {
     logOut(file + " now is download finished")
 }
 
-func outChannel() {
+func outCacheChannel() {
     i := 0
 	for {
         i++
-        logOut(fmt.Sprintf("loop: %d", i))
+        logOut(fmt.Sprintf("cache loop: %d, channel length:%d", i, len(urlChl)))
 		select {
 		    case url := <-urlChl:
-                downFile(url)
+                downChl <- url
 		}
 	}
 }
 
+func downChannel() {
+    i := 0
+    for {
+        i++
+        logOut(fmt.Sprintf("down loop: %d, channel length:%d", i, len(downChl)))
+        select {
+            case url := <-downChl:
+                downFile(url)
+        }
+    }
+}
+
 func logOut(s string) {
-    fmt.Printf("%+v\t%s\n", time.Now().UnixNano(), s)
+    log.Printf("%+v\t%s\n", time.Now().Format("2014-10-24 10:24:00"), s)
 }
 
 func main() {
@@ -76,10 +90,24 @@ func main() {
         "http://www.bz55.com/meinvbizhi/38925.html",
         "http://www.bz55.com/meinvbizhi/38872.html",
         "http://www.bz55.com/meinvbizhi/38818.html",
+        "http://www.bz55.com/meinvbizhi/38726.html",
+        "http://www.bz55.com/meinvbizhi/38444.html",
+        "http://www.bz55.com/meinvbizhi/38365.html",
+        "http://www.bz55.com/meinvbizhi/38039.html",
+        "http://www.bz55.com/meinvbizhi/25443.html",
+        "http://www.bz55.com/meinvbizhi/37989.html",
+        "http://www.bz55.com/meinvbizhi/37692.html",
+        "http://www.bz55.com/meinvbizhi/37359.html",
+        "http://www.bz55.com/meinvbizhi/36985.html",
+        "http://www.bz55.com/meinvbizhi/36876.html",
+        "http://www.bz55.com/meinvbizhi/36898.html",
+        "http://www.bz55.com/meinvbizhi/36741.html",
     }
 
-	go outChannel()
+	go outCacheChannel()
+    go downChannel()
 	urlChl = make(chan string, CHANNEL_CACHE_LENGTH)
+    downChl = make(chan string, CHANNEL_DOWN_QUEUE_LENGTH)
     for i, url := range urls {
         fmt.Printf("%v\t%#v\n", i, string(url) )
         findElement(string(url))
