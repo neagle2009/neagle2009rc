bfe2d385ccc9d7c0068895e59737fcc74ed27d5f
guchuan2009 <guchuan2009@gmail.com>
2017-03-01 20:43:09 +0800

    CHG: code format

--- auto.sh
+++ auto.sh
@@ -1,63 +1,74 @@
 #! /bin/bash
-function getBashrcName() {
-    echo ${NEAGLERC_URL}
-    return
-    if [ "$(uname)" == "Darwin" ]; then
-        echo ".profile" 
-        return 
-    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-        echo ".bashrc" 
-        return
-    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-        echo ".bashrc" 
-        return
-    fi
-        echo ".bashrc" 
-        return
+ZIP_FILE="neagle2009rc-master.zip"
+UNZIP_FOLDER=`basename "${ZIP_FILE}" ".zip"`
+ONLINE_FILE="https://github.com/neagle2009/neagle2009rc/archive/master.zip"
+SELF=$0
+
+function installZsh() {
+    wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+    test -f /bin/zsh && chsh -s /bin/zsh
+    cp -r "${UNZIP_FOLDER}/zshrc" $HOME/.zshrc
 }
-profileRc="$HOME/`getBashrcName`"
 
-# download AND unzip
-zipfile="neagle2009rc-master.zip"
-wget -O "${zipfile}" -c 'https://github.com/neagle2009/neagle2009rc/archive/master.zip' || exit
-if [ ! -f "${zipfile}" ] ;then
-    echo "not found file ${zipfile}"
-    exit
-fi
-unzip "${zipfile}"
+function getOnlinFile() {
+    wget -O "${ZIP_FILE}" -c "${ONLINE_FILE}"
+    if [ ! -f "${ZIP_FILE}" ] ;then
+        echo "not found file ${ZIP_FILE}"
+        exit
+    fi
+    unzip "${ZIP_FILE}"
+}
 
-### config bash 
-neaglerc2009Dir="neagle2009rc-master"
-cat ${neaglerc2009Dir}/bashrc_public >> ${profileRc}
-colorFile="$HMOE/.dircolors"
-if [ ! -f "${colorFile}" ] ;then
-    cat ${neaglerc2009Dir}/dir_colors >> ${colorFile}
-fi
+function installConfigFile() {
 
-hideBinDir="$HOME/.bin"
-if [ -d "${hideBinDir}" ] ;then
-    mkdir "${hideBinDir}"
-fi
+    #1. dircolor config
+    colorFile="$HMOE/.dircolors"
+    if [ ! -f "${colorFile}" ] ;then
+        cat ${UNZIP_FOLDER}/dir_colors >> ${colorFile}
+    fi
 
-### config vim
-cat ${neaglerc2009Dir}/vimrc/vimrc >> ~/.vimrc
-vimdir="$HOME/.vim"
-if [ ! -d "$vimdir" ] ;then
-    mkdir "${vimdir}"
-fi
-cp -r "${neaglerc2009Dir}/vimrc/plugin" "${vimdir}/plugin"
+    #2. install public bin
+    hideBinDir="$HOME/.bin"
+    if [ -d "${hideBinDir}" ] ;then
+        mkdir "${hideBinDir}"
+    fi
 
+    #3. install vim config
+    cat ${UNZIP_FOLDER}/vimrc/vimrc >> $HOME/.vimrc
+    vimdir="$HOME/.vim"
+    if [ ! -d "$vimdir" ] ;then
+        mkdir "${vimdir}"
+    fi
+    cp -r "${UNZIP_FOLDER}/vimrc/plugin" "${vimdir}/plugin"
+}
 
-### del file 
-rm -rf "${neaglerc2009Dir}"
-rm "$0" #delete self
+function cleanTrash() {
+    rm -rf "${UNZIP_FOLDER}"
+    rm "${SELF}"
+}
 
-##download cdargs
-zipCdargsFile="cdargs.zip"
-wget -c 'https://github.com/cbxbiker61/cdargs/archive/master.zip' -O ${zipCdargsFile}
-unzip ${zipCdargsFile}
-wget -c 'http://neagle.qiniudn.com/neagle/2014/11/29/cdargs-bash.sh'
+function downloadCdargs() {
+    zipCdargsFile="cdargs.zip"
+    wget -c 'https://github.com/cbxbiker61/cdargs/archive/master.zip' -O ${zipCdargsFile}
+    unzip ${zipCdargsFile}
+    wget -c 'http://neagle.qiniudn.com/neagle/2014/11/29/cdargs-bash.sh'
+}
 
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
-test -f /bin/zsh && chsh -s /bin/zsh
-cp -r "${neaglerc2009Dir}/zshrc" $HOME/.zshrc
+function installVundle() {
+    vimrcfile="$HOME/.vimrc"
+    vimrc=`cat ${vimrcfile}`
+    bundleVimrc=`cat ./vim_plugin_test/bundle.txt`
+    grep -q 'call vundle#begin()' ~/.vimrc || (echo "${bundleVimrc}${vimrc}" > $vimrcfile)
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
+    vim +PluginInstall ctrlp.vim +qall
+    vim +PluginInstall ctags.vim +qall
+}
+
+getOnlinFile
+installConfigFile
+installZsh
+downloadCdargs
+installVundle
+
+cleanTrash
